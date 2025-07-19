d0807dfd319f2b75ff2261025e0f05e8714e0077
Mouhamadou Ndiaye <mouhamedmbour010@gmail.com>
2025-07-19 20:17:01 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "Expo.h"
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int choix;
+    do {
+        printf("\n=== MENU BIBLIOTHEQUE ===\n");
+        printf("1. Ajouter un livre\n2. Afficher tous les livres\n3. Rechercher un livre\n4. Supprimer un livre\n5. Emprunter un livre\n6. Retourner un livre\n7. Afficher les statistiques\n0. Quitter\nChoix : ");
+        scanf("%d", &choix); getchar();
+        switch (choix) {
+            case 1: ajouterLivre();
+             break;
+
+        }
+    } while (choix != 0);
     return 0;
 }
