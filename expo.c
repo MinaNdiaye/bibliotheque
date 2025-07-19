14a174eebcc6db7bbe6d4654a9b4a2986f2fa921
Mouhamadou Ndiaye <mouhamedmbour010@gmail.com>
2025-07-19 20:15:37 +0000

    Update expo.c

---
+++ expo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "Expo.h"
+
+Livre T[MAX];
+int nbLivres = 0;
+void ajouterLivre() {
+    if (nbLivres >= MAX) return;
+    printf("Titre : ");
+    gets(T[nbLivres].titre);
+    printf("Auteur : ");
+    gets(T[nbLivres].auteur);
+    printf("Annee : "); scanf("%d", &T[nbLivres].annee);
+    printf("ISBN : "); scanf("%d", &T[nbLivres].isbn); getchar();
+    T[nbLivres].emprunte = 0;
+    nbLivres++;
+}
+
