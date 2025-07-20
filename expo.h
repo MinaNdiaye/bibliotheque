89893dcd52e818f4a131b9d5eec402d7744141c0
Mouhamadou Ndiaye <mouhamedmbour010@gmail.com>
2025-07-20 08:32:58 +0000

    Update expo.h

--- expo.h
+++ expo.h
@@ -1,16 +1,1 @@
-#ifndef EXPO_H
-#define EXPO_H
 
-#define MAX 100
-
-typedef struct {
-    char titre[100];
-    char auteur[100];
-    int annee;
-    int isbn;
-    int emprunte;
-} Livre;
-
-void ajouterLivre();
-
-#endif
