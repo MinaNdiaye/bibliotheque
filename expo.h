4a6a43f2885612e7e990ee272f3100041065d812
Mouhamadou07 <mouhamedmbour010@gmail.com>
2025-07-21 13:09:27 +0000

    ajouter des ficher main.c et expo.h

---
+++ expo.h
@@ -0,0 +1,28 @@
+#ifndef EXPO_H
+#define EXPO_H
+
+#define MAX 100
+
+typedef struct {
+    char titre[100];
+    char auteur[100];
+    int annee;
+    int isbn;
+    int emprunte;
+} Livre;
+
+typedef struct {
+    int isbn;
+    char nom_eleve[100];
+    char date[11];
+} Emprunt;
+
+void ajouterLivre();
+void afficherLivres();
+void rechercherLivre();
+void supprimerLivre();
+void emprunterLivre();
+void retournerLivre();
+void afficherStatistiques();
+
+#endif
